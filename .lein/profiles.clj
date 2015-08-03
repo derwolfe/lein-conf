42c23bfe0d4764b2acd21f17cc009c39e8fbde3c
derwolfe <chriswwolfe@gmail.com>
2015-08-02 19:03:44 -0500

    upgrade nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[lein-create-template "0.1.1"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-cljfmt "0.1.10"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
