d01064b8b3ae47f20662d5d5a07971840f274855
Chris Wolfe <chriswwolfe@gmail.com>
2016-03-15 17:27:42 -0500

    UPDATE

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,11 @@
-{:user {:plugins [[lein-create-template "0.1.1"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [lein-cljfmt "0.1.10"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+{:user {:plugins [[lein-ancient "0.6.8"]
+                  [lein-auto "0.1.2"]
+                  [lein-pprint "1.1.2"]
+                  [lein-bikeshed "0.3.0"]]
+        :dependencies [[slamhound "1.5.5"]
+                       [pjstadig/humane-test-output "0.7.1"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}
+ :repl {:plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
+                  [refactor-nrepl "2.2.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
